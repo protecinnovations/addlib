e4a6b4178d56850fee7b1d3d20242626863da9be
Jez Halford <jezhalford@protecinnovations.co.uk>
2011-08-25 14:36:56 +0100

    Added skeleton addlib class

---
+++ addlib.py
@@ -0,0 +1,25 @@
+class Addlib(object):
+
+    def __init__self():
+        self.__hostname = 'localhost'
+        self.__username = 'root'
+        self.__password = 'password'
+        self.__database = 'phasefour'    
+        self.__change_dir = '/home/users/jez/changes'
+
+    def get_next_changes():
+        """Get a list of changes that need to be applied
+        """
+
+    def deploy():
+        """Apply any waiting changes to the database
+        """
+
+    def undo(change_name):
+        """Undo the specified change
+        """
+
+    def get_dependencies(change_name):
+        """Get a list of changes that depend on the given change
+        """
+
--- scripts/create-log.sql
+++ scripts/create-log.sql
@@ -3,7 +3,7 @@
 CREATE TABLE `addlib_log` (
   `id` INT  NOT NULL AUTO_INCREMENT,
   `change_name` TEXT  NOT NULL,
-  `action` ENUM('DO', 'UNDO')  NOT NULL,
+  `action` ENUM('DO', 'UNDO') NOT NULL,
   `timestamp` INT  NOT NULL,
   `was_redo` INT(1)  NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
